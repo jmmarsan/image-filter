71063745dba8b142ba34f348d520ea98aff3b788
José María Martín Sánchez <jmmarsan@gmail.com>
2020-03-30 19:39:59 +0200

    Add typing as req: Request and res: Response

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get( "/filteredimage/", ( req, res ) => {
+  app.get( "/filteredimage/", ( req: Request, res: Response ) => {
     let { image_url } = req.query;
 
     if ( !image_url ) {
